6adc0e98ae670ef91e8099041afa45f4fafb1667
Joohee Y <jooheeyoo@gmail.com>
2024-09-21 15:00:11 -0700

    base

---
+++ app.py
@@ -0,0 +1,89 @@
+import openai
+import os
+import chainlit as cl
+from prompts import SYSTEM_PROMPT
+from dotenv import load_dotenv
+from langfuse.decorators import observe
+from langfuse.openai import openai
+
+ # Load environment variables
+load_dotenv()
+
+configurations = {
+    "mistral_7B_instruct": {
+        "endpoint_url": os.getenv("MISTRAL_7B_INSTRUCT_ENDPOINT"),
+        "api_key": os.getenv("RUNPOD_API_KEY"),
+        "model": "mistralai/Mistral-7B-Instruct-v0.2"
+    },
+    "mistral_7B": {
+        "endpoint_url": os.getenv("MISTRAL_7B_ENDPOINT"),
+        "api_key": os.getenv("RUNPOD_API_KEY"),
+        "model": "mistralai/Mistral-7B-v0.1"
+    },
+    "openai_gpt-4": {
+        "endpoint_url": os.getenv("OPENAI_ENDPOINT"),
+        "api_key": os.getenv("OPENAI_API_KEY"),
+        "model": "gpt-4"
+    }
+}
+
+# Choose configuration
+config_key = "openai_gpt-4"
+# config_key = "mistral_7B_instruct"
+#config_key = "mistral_7B"
+
+# Get selected configuration
+config = configurations[config_key]
+
+# Initialize the OpenAI async client
+client = openai.AsyncClient(api_key=config["api_key"], base_url=config["endpoint_url"])
+
+gen_kwargs = {
+    "model": config["model"],
+    "temperature": 0.3,
+    "max_tokens": 500
+}
+# Configuration setting to enable or disable the system prompt
+ENABLE_SYSTEM_PROMPT = True
+
+@observe()
+def get_latest_user_message(message_history):
+    # Iterate through the message history in reverse to find the last user message
+    for message in reversed(message_history):
+        if message['role'] == 'user':
+            return message['content']
+    return None
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    
+    message_history = cl.user_session.get("message_history", [])
+
+    if ENABLE_SYSTEM_PROMPT and (not message_history or message_history[0].get("role") != "system"):
+        system_prompt_content = SYSTEM_PROMPT
+        message_history.insert(0, {"role": "system", "content": system_prompt_content})
+
+    message_history.append({"role": "user", "content": message.content})
+    get_latest_user_message(message_history)
+    response_message = cl.Message(content="")
+
+    await response_message.send()
+
+    if config_key == "mistral_7B":
+        stream = await client.completions.create(prompt=message.content, stream=True, **gen_kwargs)
+        async for part in stream:
+            if token := part.choices[0].text or "":
+                await response_message.stream_token(token)
+    else:
+        stream = await client.chat.completions.create(messages=message_history, stream=True, **gen_kwargs)
+        async for part in stream:
+            if token := part.choices[0].delta.content or "":
+                await response_message.stream_token(token)
+
+    message_history.append({"role": "assistant", "content": response_message.content})
+    cl.user_session.set("message_history", message_history)
+    await response_message.update()
+
+
+if __name__ == "__main__":
+    cl.main()
---
+++ prompts.py
@@ -0,0 +1,17 @@
+SYSTEM_PROMPT = """
+You are a friendly math teacher for elementary school students. You help kids learn the following areas in math.
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+You have the mission of ensuring that kids do not get bored or lose interest in math. You need to create real-world questions related to topics that kids are interested in.
+
+When greeting the student, start by asking about their interests. For example, they might like soccer, pizza, games, or birds. Try to incorporate their interests into your questions. A good question for a student interested in birds could be:
+
+Question: There are 135 sparrows on the tree, a cat came and scared away 19. How many sparrows are left on the tree?
+
+This is the app for kids. It has several restrictions: 
+1. You cannot link to other websites that kids can access by clicking. 
+2. The content and language should be appropriate for children. 
+
+"""
